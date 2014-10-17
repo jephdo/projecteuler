7282efc5a86951bdb896a0da204c1a97aa695fae
Jeph Do <jephdo@gmail.com>
2014-10-16 19:14:54 -0700

    cleaned up code and comments for problem1

--- problem1.py
+++ problem1.py
@@ -1,43 +1,38 @@
-def sum_of_multiples(ceiling):
-    rolling_sum = 0
-    
-    for i in range(ceiling):
-        if (i % 3 == 0) or (i % 5 == 0):
-            rolling_sum += i
-    
-    return rolling_sum
-    
-def sum_of_multiples2(ceiling):
-    """
-    Sum of multiples of three under 10:
-    
-    3 + 6 + 9 = (3 * 1) + (3 * 2) + (3 * 3)
-              = 3 * (1 + 2 + 3)
-    """
-    def g(multiple, ceiling):
-        # subtract one because you want < not <= to ceiling
-        num_of_multiples = (ceiling-1) // multiple
-        
-        return multiple * sum(i+1 for i in range(num_of_multiples))
-    
-    return g(3, ceiling) + g(5, ceiling) - g(15, ceiling) # remove duplicates
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-def sum_of_multiples2(ceiling):
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+# def sum_of_multiples(ceiling):
+#     """Straight forward implementation"""
+#     rolling_sum = 0
+
+#     for i in range(ceiling):
+#         if (i % 3 == 0) or (i % 5 == 0):
+#             rolling_sum += i
+
+#     return rolling_sum
+
+def sum_of_multiples(ceiling):
     """
-    Sum of multiples of three under 10:
-    
+    The sum of multiples of three under a number (e.g. 10) can be factored out
+    like so:
+
     3 + 6 + 9 = (3 * 1) + (3 * 2) + (3 * 3)
               = 3 * (1 + 2 + 3)
     """
     def g(multiple, ceiling):
         # subtract one because you want < not <= to ceiling
         num_of_multiples = (ceiling-1) // multiple
-        
-        # sum of integers less than n = n(n+1)/2
+
+        # one optimization: sum of integers less than n can be expressed
+        # as n = n(n+1)/2
         return multiple * num_of_multiples * (num_of_multiples + 1) // 2
-    
-    return g(3, ceiling) + g(5, ceiling) - g(15, ceiling) # remove duplicates
+
+    # g(15) is to remove duplicates since 15 = 3 * 5
+    return g(3, ceiling) + g(5, ceiling) - g(15, ceiling)
 
 if __name__ == '__main__':
     print(sum_of_multiples(1000))
-    print(sum_of_multiples2(1000))
